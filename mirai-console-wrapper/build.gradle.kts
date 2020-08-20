5176499eefcf8c7f141f3a96488a9fa1e79403cb
gh9em <62997494+gh9em@users.noreply.github.com>
2020-08-20 13:53:07 +0800

    Update build.gradle.kts

--- mirai-console-wrapper/build.gradle.kts
+++ mirai-console-wrapper/build.gradle.kts
@@ -26,6 +26,7 @@ kotlin {
 dependencies {
     api(kotlin("stdlib", Versions.Kotlin.stdlib))
     api(kotlin("reflect", Versions.Kotlin.stdlib))
+    runtimeOnly(kotlin("serialization", Versions.Kotlin.stdlib))
 
     api(kotlinx("coroutines-core", Versions.Kotlin.coroutines))
     api(kotlinx("coroutines-swing", Versions.Kotlin.coroutines))
@@ -39,6 +40,25 @@ dependencies {
     testImplementation(kotlin("test-junit5"))
 
     // implementation(kotlin("script-runtime"))
+    runtimeOnly(kotlinx("serialization-core", Versions.Kotlin.serialization))
+    runtimeOnly(kotlinx("serialization-runtime", Versions.Kotlin.serialization))  // mirai-core-qqandroid-jvm depend
+    runtimeOnly(kotlinx("serialization-protobuf", Versions.Kotlin.serialization))  // mirai-core-qqandroid-jvm depend
+    runtimeOnly(kotlinx("io", Versions.Kotlin.io)) {
+      exclude("org.jetbrains.kotlin", "kotlin-stdlib")
+    }  // mirai-core-qqandroid-jvm depend
+    runtimeOnly(kotlinx("io-jvm", Versions.Kotlin.io)) {
+      exclude("org.jetbrains.kotlin", "kotlin-stdlib")
+    }  // mirai-core-qqandroid-jvm depend
+    runtimeOnly(kotlinx("coroutines-io", Versions.Kotlin.coroutinesIo)) {
+      exclude("org.jetbrains.kotlin", "kotlin-stdlib")
+    }  // mirai-core-qqandroid-jvm depend
+    runtimeOnly(kotlinx("coroutines-io-jvm", Versions.Kotlin.coroutinesIo)) {
+      exclude("org.jetbrains.kotlin", "kotlin-stdlib")
+    }  // mirai-core-qqandroid-jvm depend
+    runtimeOnly("org.jetbrains.kotlinx:atomicfu:" + Versions.Kotlin.atomicFU)
+    runtimeOnly("org.apache.logging.log4j:log4j-api:" + Versions.Logging.log4j)
+    runtimeOnly("org.slf4j:slf4j-api:" + Versions.Logging.slf4j)
+    runtimeOnly("org.bouncycastle:bcprov-jdk15on:1.64")  // mirai-core-jvm depend
 }
 
 version = Versions.Mirai.consoleWrapper
